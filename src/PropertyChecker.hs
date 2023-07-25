ac2939357a751fca85b4323faec2165c3521520f
Triera <tri_era13@hotmail.no>
2023-07-25 20:37:54 +0200

    generators for Integer and Boolean

--- src/PropertyChecker.hs
+++ src/PropertyChecker.hs
@@ -1,9 +1,8 @@
 
 module PropertyChecker where
 
-
 import Syntax
--- import TypeInference
+import TypeInference
 
 import Test.Tasty.QuickCheck
 
@@ -12,8 +11,13 @@ type Generator = Gen (Term Type)
 generateGenerators :: Program Type -> [Generator]
 generateGenerators _ = undefined
 
-generateGenerator :: Type -> Generator
-generateGenerator _ = undefined
+generateGenerator :: Type -> Gen (Term Type)
+generateGenerator Integer' = do
+    generatedInt <- arbitrary
+    return $ (Number generatedInt Integer')
+generateGenerator Boolean' = do
+    generatedBool <- arbitrary
+    return $ (Boolean generatedBool Boolean')
 
 -- Check takes the components of a property, and returns a generator for terms of type `Boolean'` that we can evaluate inside of QuickCheck.
 check :: [(Name, Type)] -> Term Type -> Gen [(Name, Term Type)]
