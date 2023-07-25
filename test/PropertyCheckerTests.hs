8b003ecb8c01bf0afeec289cbb8a8a65a9b8c3a0
Triera <tri_era13@hotmail.no>
2023-07-25 20:38:39 +0200

    generateGenerator test group - not tested (issues with HUnit)

---
+++ test/PropertyCheckerTests.hs
@@ -0,0 +1,19 @@
+module PropertyCheckerTests where
+
+import Syntax
+import PropertyChecker
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit
+
+{-
+generateGenerator_tests :: TestTree
+generateGenerator_tests =
+  testGroup "GenerateGenerator tests."
+    [ testCase "Generate Integer'" $
+        -- TODO: check if generatedInt has type Integer?
+        case generateGenerator Integer' of
+            (Number generatedInt Integer') -> True
+            _ -> False
+        @= True
+    ]
+-}
