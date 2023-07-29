25849cbcecb0a1c4d1af37f3750205c3bbc6e105
SophieBosio <sophie.bosio@outlook.com>
2023-07-29 21:10:25 +0200

    generateGenerator for Variable', getType helper function

--- src/PropertyChecker.hs
+++ src/PropertyChecker.hs
@@ -18,6 +18,29 @@ generateGenerator Integer' = do
 generateGenerator Boolean' = do
     generatedBool <- arbitrary
     return $ (Boolean generatedBool Boolean')
+generateGenerator (Variable' index) =
+  case infer term index of
+    (inferredTermType, _, _) ->
+      return $ (Variable varName (getType inferredTermType))
+  where
+    varName = show index
+    term    = Variable varName index
+
+getType :: Term Type -> Type
+getType (Number      _ Integer') = Integer'
+getType (Boolean     _ Boolean') = Boolean'
+getType (Variable    _ a       ) = a
+getType (If          _ _ _  tau) = tau
+getType (Plus        _ _    tau) = tau
+getType (Leq         _ _    tau) = tau
+getType (Pair        _ _    tau) = tau
+getType (Fst         _      tau) = tau
+getType (Snd         _      tau) = tau
+getType (Lambda      _ _    tau) = tau
+getType (Application _ _    tau) = tau
+getType (Let         _ _ _  tau) = tau
+getType (Rec         _ _    tau) = tau
+getType _                        = Integer'
 
 -- Check takes the components of a property, and returns a generator for terms of type `Boolean'` that we can evaluate inside of QuickCheck.
 check :: [(Name, Type)] -> Term Type -> Gen [(Name, Term Type)]
