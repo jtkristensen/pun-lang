2759660f19a8da0dbb5b5351ef61b65e67e19b79
Triera <tri_era13@hotmail.no>
2023-07-29 21:04:07 +0200

    refactoring - generator for pair and application has to be separate

--- src/PropertyChecker.hs
+++ src/PropertyChecker.hs
@@ -25,12 +25,14 @@ generateGenerator Integer' = do
 generateGenerator Boolean' = do
     generatedBool <- arbitrary
     return $ (Boolean generatedBool Boolean')
-generateGenerator (type1 symbol type2) = do
+generateGenerator (type1 :*: type2) = do
     arbitraryTerm1 <- generateGenerator type1
     arbitraryTerm2 <- generateGenerator type2
-    case symbol of
-        :*:  -> Pair arbitraryTerm1 arbitraryTerm2 (type1 :*: type2)
-        :->: -> Application arbitraryTerm1 arbitraryTerm2 (type1 :->: type2)
+    return $ Pair arbitraryTerm1 arbitraryTerm2 (type1 :*: type2)
+generateGenerator (type1 :->: type2) = do
+    arbitraryTerm1 <- generateGenerator type1
+    arbitraryTerm2 <- generateGenerator type2
+    return $ Application arbitraryTerm1 arbitraryTerm2 (type1 :->: type2)
 
 -- Check takes the components of a property, and returns a generator for terms of type `Boolean'` that we can evaluate inside of QuickCheck.
 check :: [(Name, Type)] -> Term Type -> Gen [(Name, Term Type)]
