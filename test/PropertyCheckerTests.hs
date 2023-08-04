ca1ca574943499f0d57c182a41b8ffdc9ca6f85f
Joachim <joachim.tilsted@gmail.com>
2023-08-04 17:39:50 +0200

    test case from meeting

--- test/PropertyCheckerTests.hs
+++ test/PropertyCheckerTests.hs
@@ -2,7 +2,7 @@ module PropertyCheckerTests where
 
 import Syntax
 import PropertyChecker
--- import TypeInference
+import TypeInference
 -- import Examples
 
 import Test.Tasty (TestTree, testGroup)
@@ -16,11 +16,31 @@ generateGenerator_tests :: TestTree
 generateGenerator_tests =
   testGroup "`generateGenerator` tests :"
     [ testCase "Pairs and Functions are not primitives" $
-        do
-          generatedValue <- generate $ oneof $ generateGenerator <$> [Integer', Boolean']
-          case generatedValue of
-            (Pair    _ _ _) -> False
-            (Lambda  _ _ _) -> False
-            _               -> True
-            @? (show generatedValue ++ " should have been a primitive {^_^}")
+      do generatedValue <- generate $ oneof $ generateGenerator <$> [Integer', Boolean']
+         case generatedValue of
+           (Pair    _ _ _) -> False
+           (Lambda  _ _ _) -> False
+           _               -> True
+           @? (show generatedValue ++ " should have been a primitive {^_^}")
+    , testCase "generateGenerator t has type Term t forall t." $
+      do t    <- generate aType
+         term <- generate $ generateGenerator t
+         let (t', _, cs) = infer term 0
+         let subs        = bindings cs
+         let typeOfT'    = annotation (refine subs <$> t')
+         (t == typeOfT')
+           @? ( "the term " ++ show t' ++
+                " had the type " ++ show typeOfT' ++
+                " but we expected the type " ++ show t
+              )
+    ]
+
+-- Todo, we want to use more complicated types.
+aType :: Gen Type
+aType =
+  oneof $
+    [ return Integer'
+    , return Boolean'
+    -- , ( :*:  ) <$> aType <*> aType
+    -- , ( :->: ) <$> aType <*> aType
     ]
