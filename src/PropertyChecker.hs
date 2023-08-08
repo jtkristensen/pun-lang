bdb5089b4b1a4b64153848678e4fd87b04132c42
SophieBosio <sophie.bosio@outlook.com>
2023-08-08 20:16:51 +0200

    Rename `what` to `resolve`

--- src/PropertyChecker.hs
+++ src/PropertyChecker.hs
@@ -12,16 +12,17 @@ type CurrentIndices = Substitution
 generateGenerator :: CurrentIndices -> (Type -> Generator)
 generateGenerator _ Integer'           = flip Number  Integer' <$> arbitrary
 generateGenerator _ Boolean'           = flip Boolean Boolean' <$> arbitrary
-generateGenerator s (Variable' index)  = generateGenerator s (what index s)
+generateGenerator s (Variable' index)  = generateGenerator s (resolve index s)
 -- generateGenerator s (type1 :*: type2)  = undefined
 -- generateGenerator s (type1 :->: type2) = undefined
 generateGenerator _ _ = undefined
 
 -- Todo, give it a better name {^o^}!
-what :: Index -> CurrentIndices -> Type
-what i cs = case lookup i cs of
+resolve :: Index -> CurrentIndices -> Type
+resolve i cs = case lookup i cs of
   Just s  -> s
-  Nothing -> error $ "Index " ++ show i ++ " was not among current indices" 
+  Nothing -> error $ "Unable to resolve index to type. Index "
+    ++ show i ++ " was not among current indices"
 
 -- Check takes the components of a property, and returns a generator for terms of type `Boolean'` that we can evaluate inside of QuickCheck.
 check :: [(Name, Type)] -> Term Type -> Gen [(Name, Term Type)]
