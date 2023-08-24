809ccbe360e34e2ef90f819cd908a1570f536414
Triera <tri_era13@hotmail.no>
2023-08-24 20:38:17 +0200

    find function

--- benchmark/Haskell/BST.hs
+++ benchmark/Haskell/BST.hs
@@ -21,11 +21,16 @@ insert 7 "7" (insert 4 "4" (insert 5 "5" (insert 3 "3" (Branch (Leaf) 1 "1" (Lea
 
 -- key and value we want to insert into a BST
 insert :: Ord k => k -> v -> BST k v -> BST k v
--- Todo: If null?
 insert k v Leaf = Branch (Leaf) k v (Leaf)
 insert k v (Branch left k' v' right)
     | k == k' = Branch left              k' v' right
     | k <  k' = Branch (insert k v left) k' v' right
     | k >  k' = Branch left              k' v' (insert k v right)
+insert k v _  = Branch (Leaf) k v (Leaf)
 
-    
+find :: Ord k => k -> BST k v -> Maybe v
+find k (Branch left k' v' right)
+    | k == k' = Just v'
+    | k >  k' = find k right
+    | k <  k' = find k left
+find k _                         = Nothing
