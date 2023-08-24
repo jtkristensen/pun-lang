964edb0bb6896892b942effef6293e3ce09b02f6
Triera <tri_era13@hotmail.no>
2023-08-24 22:37:17 +0200

    delete and findMin function

--- benchmark/Haskell/BST.hs
+++ benchmark/Haskell/BST.hs
@@ -33,4 +33,24 @@ find k (Branch left k' v' right)
     | k == k' = Just v'
     | k >  k' = find k right
     | k <  k' = find k left
-find k _                         = Nothing
+find k _ = Nothing
+
+findMin :: Ord k => BST k v -> k
+findMin (Branch Leaf k' _ _) = k'
+findMin (Branch left _  _ _) = findMin left
+
+{-
+delete 3
+Branch Leaf 1 "1" (Branch Leaf 3 "3" (Branch Leaf 5 "5" Leaf))
+-}
+delete :: Ord k => k -> BST k v -> BST k v
+delete _ Leaf = Leaf
+delete k tree@(Branch left k' v' right)
+    | k <  k'    = Branch (delete k left) k' v' right
+    | k >  k'    = Branch left k' v' (delete k right)
+    | otherwise  = delete' tree
+
+delete' :: Ord k => BST k v -> BST k v
+delete' (Branch Leaf _  _  right) = right
+delete' (Branch left _  _  Leaf ) = left
+delete' (Branch left k' v' right) = Branch left (findMin right) v' (delete k' right)
